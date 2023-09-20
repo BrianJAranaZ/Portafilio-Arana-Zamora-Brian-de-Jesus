2f46342d35ec9d410efd242454db4ab003a15e55
BrianJAranaZ <108771137+BrianJAranaZ@users.noreply.github.com>
2023-09-19 23:18:12 -0500

    Delete actividad1 directory

--- actividad1/seis/holanombre.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "Hola mi nombre es: Arana Zamora Brian de Jesus"
--- actividad1/seis/mundovariable18.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# En la primera linea establezco con qué shell se debe ejecutar
-# Se define una variable
-SALUDO="Hola mundo"
